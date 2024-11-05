5cf9ad2494dff47599bee507cc98b0bd37793ca8
fatken08xp <131841477+fatken08xp@users.noreply.github.com>
2024-11-05 19:46:51 +0800

    Update Streamlit_Script.py

--- Streamlit_Script.py
+++ Streamlit_Script.py
@@ -115,8 +115,7 @@ if st.button("Check Infringement") and patent_id and company_name:
                     "role": "user",
                     "content": prompt,
                 },
-            max_tokens ==5000,
-            temperature==0.3
+            temperature=0.3
             ]
         )
         # Extract and display the generated response
